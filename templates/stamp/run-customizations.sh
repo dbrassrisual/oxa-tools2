00a255ac758a1956916b2dea215aafd4cf6e4cbe
risualSupport <30312631+risualSupport@users.noreply.github.com>
2017-09-07 10:04:00 +0100

    Update run-customizations.sh

--- templates/stamp/run-customizations.sh
+++ templates/stamp/run-customizations.sh
@@ -728,4 +728,7 @@ log "Uninstalling run-customization background installer cron job"
 crontab -l | grep -v "sudo bash $CRON_INSTALLER_SCRIPT" | crontab -
 
 log "risual customisation time!"
-sudo mkdir /etc/hel
+sudo git clone --branch oxa/master.fic https://github.com/risualSupport/Customfiles.git /etc/risualCustom
+log "Cloning custom repo"
+sudo bash /etc/risualCustom/CopyLmsEnv.sh
+log "Ran custom script"
